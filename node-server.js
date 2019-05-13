b30088f91639ceb004a19e7e280a4177a4b79669
SATYAM <satyam.dev29@gmail.com>
2019-05-13 12:31:46 +0530

    Setup server with node

--- node-server.js
+++ node-server.js
@@ -1,64 +1,30 @@
-const http = require('http') // http is module. First we'll require http module and store in one varaible that is called http
-const port = 3000 // Set port number
-
-// http variable have createServer object which will help to setup server and function will return and store in server variable.
+const http = require('http')
+const port = 3000
 
 const server = http.createServer(function(request, response){
 if (request.url =='/'){
-    response.end('Hello World') 
+    response.end('Hello World')
 }else if(request.url=='/about'){
     response.end('This is about page')
 } else if(request.url=='/contact'){
     response.end('Contact page')
 } else if(request.url=='/users'){
     response.status = 200
- response.setHeader('Content-Type', 'text/html') // setting header content to understand and execute html element and render on page. If we'll not set Header then we'll get plain text.
+ response.setHeader('Content-Type', 'text/html')
  response.end('<ul><li>Ramesh</li><li>Suresh</li></ul')
 } else if(request.url=='/products'){
     const products = [
         {id:1, name:'marker'},
-        {id:2, name:'scale'}const http = require('http')
-        const port = 3000
-        
-        const server = http.createServer(function(request, response){
-        if (request.url =='/'){
-            response.end('Hello World')
-        }else if(request.url=='/about'){
-            response.end('This is about page')
-        } else if(request.url=='/contact'){
-            response.end('Contact page')
-        } else if(request.url=='/users'){
-            response.status = 200
-         response.setHeader('Content-Type', 'text/html')
-         response.end('<ul><li>Ramesh</li><li>Suresh</li></ul')
-        } else if(request.url=='/products'){
-            const products = [
-                {id:1, name:'marker'},
-                {id:2, name:'scale'}
-            ]
-            response.setHeader('Content-Type', 'application/json')
-            response.end(JSON.stringify(products))
-        }
-        else{
-            response.status = 404
-            response.end('404 Page not found..')
-        }
-        })
-        
-        server.listen(port,function(){
-            console.log('listening on port', port)
-        })
+        {id:2, name:'scale'}
     ]
-    response.setHeader('Content-Type', 'application/json') // set header to get json data.
-    response.end(JSON.stringify(products)) // parse jsone data and convert to string to read object.
+    response.setHeader('Content-Type', 'application/json')
+    response.end(JSON.stringify(products))
 }
 else{
     response.status = 404
     response.end('404 Page not found..')
 }
 })
-
-// listening port number
 
 server.listen(port,function(){
     console.log('listening on port', port)
