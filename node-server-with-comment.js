04032b8998ccbab3733321e47253cce414ee982e
SATYAM <satyam.dev29@gmail.com>
2019-05-13 12:29:57 +0530

    Setup server with node

--- node-server-with-comment.js
+++ node-server-with-comment.js
@@ -17,37 +17,7 @@ if (request.url =='/'){
 } else if(request.url=='/products'){
     const products = [
         {id:1, name:'marker'},
-        {id:2, name:'scale'}const http = require('http')
-        const port = 3000
-        
-        const server = http.createServer(function(request, response){
-        if (request.url =='/'){
-            response.end('Hello World')
-        }else if(request.url=='/about'){
-            response.end('This is about page')
-        } else if(request.url=='/contact'){
-            response.end('Contact page')
-        } else if(request.url=='/users'){
-            response.status = 200
-         response.setHeader('Content-Type', 'text/html')
-         response.end('<ul><li>Ramesh</li><li>Suresh</li></ul')
-        } else if(request.url=='/products'){
-            const products = [
-                {id:1, name:'marker'},
-                {id:2, name:'scale'}
-            ]
-            response.setHeader('Content-Type', 'application/json')
-            response.end(JSON.stringify(products))
-        }
-        else{
-            response.status = 404
-            response.end('404 Page not found..')
-        }
-        })
-        
-        server.listen(port,function(){
-            console.log('listening on port', port)
-        })
+        {id:2, name:'scale'}
     ]
     response.setHeader('Content-Type', 'application/json') // set header to get json data.
     response.end(JSON.stringify(products)) // parse jsone data and convert to string to read object.
